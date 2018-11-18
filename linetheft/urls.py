1ec87b5bdd6319aa91dbffd6979c50f3a7976e3d
Pankaj Dahilkar <pankajmdahilkar@gmail.com>
2018-11-18 13:08:53 +0530

    some changes

--- linetheft/urls.py
+++ linetheft/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url,include
+from .import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^energyapp/',include('energyapp.urls'))
+    url(r'^energyapp/',include('energyapp.urls')),
+    url(r'^$', views.hello_world),
+
 ]
---
+++ linetheft/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+def hello_world(request):
+    return render(request,'home.html')
